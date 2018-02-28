1db26b6aca61276c71cef5bf04624628916db286
Yuvrender Singh Gill <yuvrender.gill@mail.utoronto.ca>
2018-02-27 19:20:40 -0500

    working on preprocessor

--- lm_train.py
+++ lm_train.py
@@ -1,4 +1,5 @@
 from preprocess import *
+from multiprocessing import Pool, Process, Manager
 import pickle
 import os
 
@@ -25,11 +26,48 @@ def lm_train(data_dir, language, fn_LM):
 	e.g., LM['uni']['word'] = 5 		# The word 'word' appears 5 times
 		  LM['bi']['word']['bird'] = 2 	# The bigram 'word bird' appears 2 times.
     """
-	
-	# TODO: Implement Function
 
-    #Save Model
+
+
+    unigram_dict = {}
+    bigram_dict = {}
+
+    language_model = {"uni": unigram_dict, "bi": bigram_dict}
+
+#Save Model
     with open(fn_LM+'.pickle', 'wb') as handle:
         pickle.dump(language_model, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        
     return language_model
+
+def unigram_dict(dictionary, fname):
+    print("Processing " + fname)
+    file = open(fname, "r")
+
+    for line in file.readlines():
+        for word in preprocess(line, "e").split():
+            if (word in dictionary):
+                dictionary[word] += 1
+            else:
+                dictionary[word] = 1
+    file.close()
+    return dictionary
+
+if __name__ == "__main__":
+    indir = "./data/"
+    with Manager() as manager:
+        dict = manager.dict()
+    for subdir, dirs, files in os.walk(indir):
+        for file in files:
+
+            fullFile = os.path.join(subdir, file)
+            p = Process(target=unigram_dict, args=(dict, fullFile))
+            print(dict)
+            p.start()
+            p.join()
+
+
+
+
+
+
+
--- preprocess.py
+++ preprocess.py
@@ -2,27 +2,27 @@ import re
 
 
 """ Global Variables """
-abbrivaition_file_path1 = "/h/u1/cs401/Wordlists/abbrev.english"
-abbrivaition_file_path2 = "/h/u1/cs401/Wordlists/abbrev.english~"
-abbrivaition_file_path3 = "/h/u1/cs401/Wordlists/pn_abbrev.english"
-abbrivaition_file_path4 = "/h/u1/cs401/Wordlists/pn_abbrev.english2"
-abbrivaition_file1 = open(abbrivaition_file_path1, 'r')
-abbrivaition_file2 = open(abbrivaition_file_path2, 'r')
-abbrivaition_file3 = open(abbrivaition_file_path3, 'r')
-abbrivaition_file4 = open(abbrivaition_file_path4, 'r')
-abbrivaition_list = set(abbrivaition_file1.readlines()).union(
-    set(abbrivaition_file2.readlines()).union(set(abbrivaition_file3.readlines()).union(
-set(abbrivaition_file4.readlines())
-    )))
-abbrivaition_file1.close()
-abbrivaition_file2.close()
-abbrivaition_file3.close()
-abbrivaition_file4.close()
+# abbrivaition_file_path1 = "/h/u1/cs401/Wordlists/abbrev.english"
+# abbrivaition_file_path2 = "/h/u1/cs401/Wordlists/abbrev.english~"
+# abbrivaition_file_path3 = "/h/u1/cs401/Wordlists/pn_abbrev.english"
+# abbrivaition_file_path4 = "/h/u1/cs401/Wordlists/pn_abbrev.english2"
+# abbrivaition_file1 = open(abbrivaition_file_path1, 'r')
+# abbrivaition_file2 = open(abbrivaition_file_path2, 'r')
+# abbrivaition_file3 = open(abbrivaition_file_path3, 'r')
+# abbrivaition_file4 = open(abbrivaition_file_path4, 'r')
+# abbrivaition_list = set(abbrivaition_file1.readlines()).union(
+#     set(abbrivaition_file2.readlines()).union(set(abbrivaition_file3.readlines()).union(
+# set(abbrivaition_file4.readlines())
+#     )))
+# abbrivaition_file1.close()
+# abbrivaition_file2.close()
+# abbrivaition_file3.close()
+# abbrivaition_file4.close()
 
 
 def preprocess(in_sentence, language):
     """ 
-    This function preprocesses the input text according to language-specific rules.
+    This function pre-processes the input text according to language-specific rules.
     Specifically, we separate contractions according to the source language, convert
     all tokens to lower-case, and separate end-of-sentence punctuation 
 	
@@ -53,10 +53,10 @@ def punctuation_processor(in_sentence):
     @param String comment: a String to tokenize punctuation from
     @rtype: String
     '''
-    abbr_list = abbrivaition_list
-        # ['Capt', 'Col.', 'Dr.', 'Drs.', 'Fig.', 'Figs.', 'Gen.',
-        #          'Mr.', 'Mrs.', 'Ref.', 'Rep.', 'Reps.', 'Sen.', 'fig.',
-        #          'figs.', 'vs.', 'Lt.', 'e.g.', 'i.e.']
+    abbr_list = \
+        ['Capt', 'Col.', 'Dr.', 'Drs.', 'Fig.', 'Figs.', 'Gen.',
+                 'Mr.', 'Mrs.', 'Ref.', 'Rep.', 'Reps.', 'Sen.', 'fig.',
+                 'figs.', 'vs.', 'Lt.', 'e.g.', 'i.e.']
 
     lst_str = in_sentence.split()
     out_sentence = ""
